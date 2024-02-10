dcaffa2da6f60975fa3e8feb997f08af7862cf39
Esther2524 <wangzhixiao2020@163.com>
2024-02-09 20:40:47 -0800

    Finished basic code

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 import StartScreen from './screens/StartScreen';
 import AddActivity from './screens/AddActivity';
-import AllActivities from './screens/AllActivities';
-import SpecialActivities from './screens/SpecialActivities';
 import ActivitiesProvider from './context/ActivitiesProvider';
 import { NavigationContainer } from '@react-navigation/native';
-import Colors from './Colors';
+import { Colors } from './Theme';
+import ActivityTab from './components/ActivityTab';
 // App.js will set up the navigation structure, 
 // creating a stack navigator that includes the start screen and a bottom tab navigator for the activity screens.
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function ActivityTab() {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.header }, // Set the background color for all headers
-        headerTintColor: Colors.headerBackground, // Optional: Set the color of the header title and icons
-      }}
-    >
-      <Tab.Screen
-        name="AllActivities"
-        component={AllActivities}
-        options={{ headerTitle: "All Activities" }} />
-
-      <Tab.Screen
-        name="SpecialActivities"
-        component={SpecialActivities}
-        options={{ headerTitle: "All Activities" }} />
-
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
 
