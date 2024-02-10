f18503cb0ee07afd14df023ae610f642b2d56c64
Esther2524 <wangzhixiao2020@163.com>
2024-02-10 14:19:32 -0800

    Adjusted style values based on Theme.js file

--- components/ActivitiesList.js
+++ components/ActivitiesList.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import React from 'react';
 import { ActivitiesContext } from '../context/ActivitiesProvider';
 import { useContext } from 'react';
-import { Colors } from '../Theme';
+import { Colors, fontSize, Padding } from '../Theme';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function ActivitiesList({ showSpecialOnly }) {
@@ -11,7 +11,6 @@ export default function ActivitiesList({ showSpecialOnly }) {
 
   console.log(activities);
 
-
   function renderActivity({ item }) {
     return (
       <View style={styles.activityContainer}>
@@ -19,12 +18,12 @@ export default function ActivitiesList({ showSpecialOnly }) {
           <Text style={styles.typeText}>{item.type}</Text>
         </View>
         <View style={styles.iconAndTextContainer}>
-        {item.isSpecial ? (
-          <AntDesign style={styles.icon} name={'star'} size={22} color={Colors.activeTab} />
-        ) : (
-          // placeholder view when there is no icon
-          <View style={[styles.icon, { width: 22, height: 22 }]} />
-        )}
+          {item.isSpecial ? (
+            <AntDesign style={styles.icon} name={'star'} size={22} color={Colors.activeTab} />
+          ) : (
+            // placeholder view when there is no icon
+            <View style={[styles.icon, { width: 22, height: 22 }]} />
+          )}
           <View style={styles.textContainer}>
             <Text style={styles.dateText}>{item.date}</Text>
             <Text style={styles.durationText}>{item.duration} min</Text>
@@ -42,7 +41,6 @@ export default function ActivitiesList({ showSpecialOnly }) {
   return (
     <View style={styles.container}>
       <FlatList
-        style={styles.scrollView}
         data={filteredActivities}
         renderItem={renderActivity}
         keyExtractor={item => item.id}
@@ -55,28 +53,24 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 30,
   },
-  scrollView: {
-
-  },
   activityContainer: {
     marginBottom: 10,
     marginTop: 10,
     marginRight: 20,
     marginLeft: 20,
-    paddingTop: 10,
-    paddingBottom: 10,
+    paddingTop: Padding.containerPadding,
+    paddingBottom: Padding.containerPadding,
     borderBottomWidth: 1,
     flexDirection: 'row',
-    // justifyContent: 'space-around',
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: Colors.itemBackground,
     borderRadius: 10,
   },
   typeText: {
-    fontSize: 16,
+    fontSize: fontSize.activityText,
     fontWeight: 'bold',
-    color: 'white',
+    color: Colors.itemName,
     marginLeft: 5,
     marginRight: 5,
   },
@@ -93,26 +87,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   durationText: {
-    fontSize: 16,
+    fontSize: fontSize.activityText,
     fontWeight: 'bold',
     backgroundColor: Colors.itemTexBackground,
-    padding: 5,
+    padding: Padding.activityPadding,
     color: Colors.text,
     borderRadius: 5, // set the roundness for the text area
     overflow: 'hidden', // ensure the background doesn't spill outside the border radius
   },
   dateText: {
-    fontSize: 16,
+    fontSize: fontSize.activityText,
     fontWeight: 'bold',
     backgroundColor: Colors.itemTexBackground,
-    padding: 5,
+    padding: Padding.activityPadding,
     color: Colors.text,
     borderRadius: 5,
     overflow: 'hidden',
     marginRight: 8,
   },
   icon: {
-    padding: 5,
+    padding: Padding.activityPadding,
     marginRight: 5,
   }
 
--- components/Button.js
+++ components/Button.js
@@ -1,31 +1,30 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { Colors } from '../Theme';
+import { Colors, fontSize } from '../Theme';
 
 export default function Button({ onPress, title, disabled, textColor }) {
-    return (
-        <TouchableOpacity
-            onPress={onPress}
-            style={styles.button}
-            disabled={disabled}
-        >
-            <Text
-                style={[styles.text, { color: disabled ? Colors.disabledButton : textColor }]}
-            >
-                {title}
-            </Text>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.button}
+      disabled={disabled}
+    >
+      <Text
+        style={[styles.text, { color: disabled ? Colors.disabledButton : textColor }]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 16,
-        textAlign: 'center',
-        fontWeight: 'bold',
-    },
-    button: {
-        marginRight: 10,
-        // backgroundColor: 'yellow',
-    }
+  text: {
+    fontSize: fontSize.buttonText,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  button: {
+    marginRight: 10,
+  }
 })
