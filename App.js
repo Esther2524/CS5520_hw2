945ac24dc73db1a9505c64ca5e90eea84ccda9ed
Esther2524 <wangzhixiao2020@163.com>
2024-02-09 21:02:15 -0800

    Separated StackNavigator component and move the stack navigation logic out of App.js

--- App.js
+++ App.js
@@ -1,60 +1,15 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
-import StartScreen from './screens/StartScreen';
-import AddActivity from './screens/AddActivity';
 import ActivitiesProvider from './context/ActivitiesProvider';
-import { NavigationContainer } from '@react-navigation/native';
-import { Colors } from './Theme';
-import ActivityTab from './components/ActivityTab';
+import StackNavigator from './navigation/StackNavigator';
 // App.js will set up the navigation structure, 
 // creating a stack navigator that includes the start screen and a bottom tab navigator for the activity screens.
 
-const Stack = createNativeStackNavigator();
+// the navigation logic is now encapsulated within the StackNavigator 
 
 export default function App() {
 
   return (
     <ActivitiesProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.headerBackground }, // Global header style
-            headerTintColor: Colors.header,
-          }}>
-          <Stack.Screen
-            name="Start"
-            component={StartScreen}
-            options={{ headerShown: false }} />
-
-          <Stack.Screen
-            name="Activities"
-            component={ActivityTab}
-            options={{ headerShown: false }} />
-
-          <Stack.Screen
-            name="AddActivities"
-            component={AddActivity}
-            options={{ headerTitle: "Add Activity" }} />
-
-        </Stack.Navigator>
-      </NavigationContainer>
-
+      <StackNavigator />
     </ActivitiesProvider>
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-});
