81c645ffe3781a77b0ebfd27f30049171487716e
Esther2524 <wangzhixiao2020@163.com>
2024-02-24 21:37:37 -0800

    Changed the color to a constant

--- components/PressableButton.js
+++ components/PressableButton.js
@@ -13,7 +13,7 @@ export default function PressableButton({ onPress, customStyle, children, disabl
           onPress();
         }
       }}
-      android_ripple={{color: 'lightgrey', borderless: false}} // apply the ripple effect for Android
+      android_ripple={{color: Colors.androidRipple, borderless: false}} // apply the ripple effect for Android
       style={({ pressed }) => [
         styles.defaultStyle,
         customStyle,
